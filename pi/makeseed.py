0c15e79721f41b8cee9a29565135dda56b4abc61
H. Watanabe <kaityo@users.sourceforge.jp>
2020-06-17 23:13:26 +0900

    adds pi

---
+++ pi/makeseed.py
@@ -0,0 +1,14 @@
+N = 20
+
+for i in range(N):
+    filename = "seed{:02d}.dat".format(i)
+    print(filename)
+    with open(filename, "w") as f:
+        f.write(str(i))
+
+with open("task.sh", "w") as f:
+    print("task.sh")
+    for i in range(N):
+        filename = "seed{:02d}.dat".format(i)
+        result = "result{:02d}.dat".format(i)
+        f.write("python pi.py < {} > {}\n".format(filename, result))
