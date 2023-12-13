ad95ef1edaf084f87fa0e38c62bb01956626a77b
Ryo Yuse <ryo1230cookie@gmail.com>
2023-12-13 22:56:00 +0900

    update

--- pi/test_prediction.py
+++ pi/test_prediction.py
@@ -18,12 +18,12 @@ except ImportError:
 
 def make_board():
     board = []
-    board_1 = [0, 1, 0, 0, -2, 0]
-    board_2 = [0, 0, 1, 0, -1, 0]
+    board_1 = [0, 0, 0, 0, -2, 0]
+    board_2 = [0, 0, 0, 0, 0, 0]
     board_3 = [0, 2, 0, -2, 0, 0]
-    board_4 = [0, 0, -1, 0, 2, 0]
-    board_5 = [0, 0, 2, -2, 0, 0]
-    board_6 = [0, 1, 0, 0, 0, 0]
+    board_4 = [0, 0, 0, 0, 0, 0]
+    board_5 = [-1, 0, 2, -2, 0, 0]
+    board_6 = [0, 1, 0, 1, 0, -1]
     board.append(board_1)
     board.append(board_2)
     board.append(board_3)
@@ -31,7 +31,7 @@ def make_board():
     board.append(board_5)
     board.append(board_6)
     print(board)
-    return np.array(board)
+    return board
 
 def load_model():
     model = create_model()
@@ -65,22 +65,16 @@ def make_one_hot(board):
     print(one_hot)
     return one_hot
 
-def main(a):
+def main():
     test_model = load_model()
     test_board = make_board()
     test_one_hot = make_one_hot(test_board)
     test_test = []
     test_test.append(test_one_hot)
     test_test_test = np.array(test_test)
-    if a == 1:
-        prediction = test_model.predict(test_test_test)[0]
-        print("/////////")
-        print(prediction)
-    elif a == 2:
-        prediction = test_model.predict(test_test_test)
-        print("/////////")
-        print(prediction)    
+    prediction = test_model.predict(test_test_test)[0]
+    print("/////////")
+    print(prediction)   
     return
 
-a = int(sys.argv[1])
-main(a)
+main()
